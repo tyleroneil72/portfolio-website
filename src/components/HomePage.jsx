172eb13b8054f51d37f45b6f3be6870c44565c1f
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 17:37:07 -0500

    Add photo

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import Typewriter from "./effects/Typewriter";
+import headshot from "../assets/headshot.jpg";
 
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
       <Typewriter text='Welcome to My Website' delay={100} infinite={false} />
+      <img src={headshot} alt='Headshot photo of Tyler Oneil' />
     </div>
   );
 }
