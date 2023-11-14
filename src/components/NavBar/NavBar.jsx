74c39082a4f411367a2b90d7bc018f858acc925b
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 14:37:37 -0500

    Changed nav header icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { IconContext } from "react-icons";
 import { SiGithub } from "react-icons/si";
 import { SlUser, SlHome } from "react-icons/sl";
 import { FaAddressCard } from "react-icons/fa6";
@@ -5,25 +6,16 @@ import { IoIosPaper } from "react-icons/io";
 import NavItem from "./NavItem";
 import { TfiWrite } from "react-icons/tfi";
 import { LuBinary } from "react-icons/lu";
+import { PiBracketsCurlyFill } from "react-icons/pi";
 // Icons from https://react-icons.github.io/react-icons/
 const NavBar = ({ selected, setSelected }) => {
   return (
     <nav className='h-screen w-fit bg-slate-950 p-4 flex flex-col items-center gap-2'>
-      {/* This svg is the top icon must change to something better */}
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='24'
-        height='24'
-        viewBox='0 0 24 24'
-        fill='none'
-        stroke='currentColor'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-      >
-        <circle cx='12' cy='7' r='4' />
-        <path d='M2 20s0-4 4-4h12s4 0 4 4' />
-      </svg>
+      <IconContext.Provider value={{ className: "w-11 h-11" }}>
+        <div>
+          <PiBracketsCurlyFill />
+        </div>
+      </IconContext.Provider>
 
       <NavItem selected={selected === 0} id={0} setSelected={setSelected}>
         <SlHome />
