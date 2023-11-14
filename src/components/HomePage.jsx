2bea788fef9dfcba242b9ff2d93f8eedac2a735c
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 18:59:40 -0500

    Commented out image

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,14 +3,17 @@ import headshot from "../assets/headshot.jpg";
 
 function HomePage() {
   return (
-    <div className='max-w-md mx-auto'>
-      <h1 className='text-4xl font-bold text-slate-100'>Home Page</h1>
-      <Typewriter
-        text='Welcome to My Website...'
-        delay={100}
-        infinite={false}
-      />
-    </div>
+    <>
+      {/* <img className='h-20 w-20' src={headshot} alt='' /> Needs resizing and formatting*/}
+      <div className='max-w-md mx-auto'>
+        <h1 className='text-4xl font-bold text-slate-100'>Home Page</h1>
+        <Typewriter
+          text='Welcome to My Website...'
+          delay={100}
+          infinite={false}
+        />
+      </div>
+    </>
   );
 }
 
