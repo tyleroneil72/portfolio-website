09b91cc26c14b497a8cc78bff9092d15aefc776d
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 17:00:20 -0500

    Created Typewriter component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,10 @@
+import Typewriter from "./effects/Typewriter";
+
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Typewriter text='Welcome to My website' delay={100} infinite={false} />
     </div>
   );
 }
