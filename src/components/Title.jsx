a3b21a5037e95b2cb9b997760a9a971f23eb96ed
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 22:59:06 -0500

    Changed text speed

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({ title }) => {
   return (
     <div className='h-screen w-screen flex flex-col items-center'>
       <h1 className='text-4xl font-bold text-slate-100 mt-16 mb-4 text-center'>
-        <Typewriter text={title} delay={100} infinite={false} />
+        <Typewriter text={title} delay={70} infinite={false} />
       </h1>
     </div>
   );
