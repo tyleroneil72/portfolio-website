72f7b7e38c3b1bf64081ebcc138855060771c299
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 19:34:58 -0500

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className='fixed bottom-0 w-full flex justify-center'>
-      <div className=''>&copy; Tyler O'Neil</div>
+    <footer className='fixed bottom-2 right-10 max-w-md mx-auto'>
+      <p className='text-slate-100'>&copy; Tyler O'Neil</p>
     </footer>
   );
 };
