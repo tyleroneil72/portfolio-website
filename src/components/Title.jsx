c4ca71fed7159b9fac7ad9d338086c4f53152fa6
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-15 13:20:28 -0500

    Changed title location

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,8 +2,8 @@ import Typewriter from "./effects/Typewriter";
 
 const Title = ({ title }) => {
   return (
-    <div className='h-screen w-screen flex flex-col items-center'>
-      <h1 className='text-4xl font-bold text-slate-100 mt-16 mb-4 text-center'>
+    <div className='h-screen w-screen flex flex-col'>
+      <h1 className='text-4xl font-bold text-slate-100 mt-8 mb-4 ml-4'>
         <Typewriter text={title} delay={70} infinite={false} />
       </h1>
     </div>
