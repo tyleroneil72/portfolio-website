0f45e11edfe3919c0dc3dda83a3ad9c6c956d91f
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-14 20:28:20 -0500

    add comment

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,16 +3,20 @@ import headshot from "../assets/headshot.jpg";
 
 function HomePage() {
   return (
-    <div className='h-screen w-screen flex flex-col items-center'>
-      <h1 className='text-4xl font-bold text-slate-100 mt-16 mb-4 text-center'>
-        Home Page
-      </h1>
-      <Typewriter
-        text='Welcome to My Website...'
-        delay={100}
-        infinite={false}
-      />
-    </div>
+    <>
+      <div className='h-screen w-screen flex flex-col items-center'>
+        <h1 className='text-4xl font-bold text-slate-100 mt-16 mb-4 text-center'>
+          Home Page
+        </h1>
+        <Typewriter
+          text='Welcome to My Website...'
+          delay={100}
+          infinite={false}
+        />
+      </div>
+
+      {/* Make the title into a header component then create anothe component below it that has the correct css so things can be put in it */}
+    </>
   );
 }
 
