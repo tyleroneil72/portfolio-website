df54eb64533d99e763188a28b60df35a79943fb3
Tyler O'Neil <tyleroneildev@gmail.com>
2023-11-15 13:13:52 -0500

    Added year to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='fixed bottom-2 right-10 max-w-md mx-auto'>
-      <p className='text-slate-100'>&copy; Tyler O'Neil</p>
+      <p className='text-slate-100'>&copy; Tyler O'Neil 2023</p>
     </footer>
   );
 };
