cbbfb84dad859f3b982d8cd7bfce9bf2ae854475
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-06 03:57:17 -0500

    Add Footer TODO

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 const Footer = () => {
   return (
+    // TODO: Fix footer to look nicer and have content
     <footer className='fixed bottom-2 right-10 max-w-md mx-auto'>
       <p className='text-slate-100'>&copy; Tyler O'Neil 2023</p>
     </footer>
