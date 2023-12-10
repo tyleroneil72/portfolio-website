841c9acd05e96d8d3a2f4c9ce7c025619a3a749d
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-10 04:20:45 -0500

    Delete Filler Code

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -18,19 +18,6 @@ const AboutMe = () => {
         }`}
       >
         <p className='mt-2'>Coming Soon...</p>
-        {/* This doesnt look that good but its a good start */}
-        {/* <div className='bg-gray-100 p-6 rounded-lg shadow-md text-gray-800'>
-          <p className='text-lg font-semibold mb-4'>
-            Highly motivated and aspiring Software Developer
-          </p>
-          <p className='text-base'>
-            Skilled in a wide range of programming languages and technologies,
-            with a passion for creating intuitive user experiences. Currently
-            pursuing an Advanced Diploma in Computer Programming and Analysis at
-            George Brown College. Previously attended The University of Toronto
-            majoring in Computer Science and Mathematics.
-          </p>
-        </div> */}
       </div>
     </>
   );
