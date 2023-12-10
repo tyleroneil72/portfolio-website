849ebaccdfd50df410be78ecfb29f4f3c1b8a2a9
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-10 04:32:20 -0500

    Put Basic Skills

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -17,7 +17,22 @@ const Skills = () => {
           fadeIn ? "opacity-100" : "opacity-0"
         }`}
       >
-        <p className='mt-2'>Coming Soon...</p>
+        <p className='mt-2 mb-5'>Coming Soon...</p>
+        <p className='mt-2 text-lg leading-relaxed'>
+          <strong className='text-blue-600'>Languages:</strong>{" "}
+          Javascript/Typescript, Python, Java, C#, Bash, PHP, SQL
+          <br />
+          <strong className='text-blue-600'>
+            Technologies and Frameworks:
+          </strong>{" "}
+          Node.js, Express.js, React.js, Vite, Tailwind, Bootstrap, .NET
+          <br />
+          <strong className='text-blue-600'>Databases:</strong> MySQL, Snowflake
+          <br />
+          <strong className='text-blue-600'>Development Tools:</strong> AWS,
+          Linux, Git/Github, Firebase Hosting, Wordpress, Jira, Confluence,
+          Slack
+        </p>
       </div>
     </>
   );
