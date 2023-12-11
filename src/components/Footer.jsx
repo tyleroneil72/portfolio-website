86f0dff9f9b7587fa20439389fbfa1307f3338a1
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-11 07:47:59 -0500

    Add Footer Background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     // TODO: Fix footer to look nicer and have content
-    <footer className='fixed bottom-2 right-10 max-w-md mx-auto'>
+    <footer className='fixed bottom-2 right-10 max-w-md mx-auto bg-slate-950 rounded-md pl-4 pr-4'>
       <p className='text-slate-100'>&copy; Tyler O'Neil 2023</p>
     </footer>
   );
