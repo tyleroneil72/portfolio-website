94bf3482bf327cb18d064976b8d0ccc8fcaee035
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-11 07:49:20 -0500

    Make Year Dynamic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     // TODO: Fix footer to look nicer and have content
     <footer className='fixed bottom-2 right-10 max-w-md mx-auto bg-slate-950 rounded-md pl-4 pr-4'>
-      <p className='text-slate-100'>&copy; Tyler O'Neil 2023</p>
+      <p className='text-slate-100'>&copy; Tyler O'Neil {currentYear}</p>
     </footer>
   );
 };
