ed622d70b62baf8b47881e2379843b06091755f8
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-17 15:42:33 -0500

    Add back Selected Box

--- src/components/NavBar/NavItem.jsx
+++ src/components/NavBar/NavItem.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const NavItem = ({ children, selected, id, setSelected, title }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -18,10 +18,27 @@ const NavItem = ({ children, selected, id, setSelected, title }) => {
       whileTap={{ scale: 0.95 }}
     >
       <span className='block relative z-10'>{children}</span>
+      <AnimatePresence>
+        {selected && (
+          <motion.span
+            key='highlight'
+            className='absolute inset-0 rounded-md bg-indigo-600 z-0'
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0 }}
+          ></motion.span>
+        )}
+      </AnimatePresence>
       {isHovered && (
-        <div className='fixed top-0 left-full ml-4 bg-gray-400 text-white rounded-md shadow-md px-3 py-2 z-50 max-w-xs truncate'>
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -20 }}
+          transition={{ duration: 0.3, ease: "easeOut" }}
+          className='fixed top-0 left-full ml-4 bg-gray-400 text-white rounded-md shadow-md px-3 py-2 z-50 max-w-xs truncate'
+        >
           {title}
-        </div>
+        </motion.div>
       )}
     </motion.button>
   );
