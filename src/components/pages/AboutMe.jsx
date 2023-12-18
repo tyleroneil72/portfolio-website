188de0f76f1abccf778efebb4deb11e5c6e8686c
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-18 07:09:01 -0500

    Add Resume Button

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -84,7 +84,20 @@ const AboutMe = () => {
               &nbsp;LinkedIn
             </a>
           </div>
+
+          <a
+            href='/resume.pdf'
+            className='relative px-5 py-2 font-medium text-white group mt-4'
+            target='_blank'
+          >
+            <span className='absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-indigo-500 group-hover:bg-indigo-700 group-hover:skew-x-12'></span>
+            <span className='absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-indigo-700 group-hover:bg-indigo-500 group-hover:-skew-x-12'></span>
+            <span className='absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-indigo-600 -rotate-12'></span>
+            <span className='absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-indigo-400 -rotate-12'></span>
+            <span className='relative'>View Resume</span>
+          </a>
         </div>
+
         <div className='border-2 border-gray-300 p-4 w-full md:w-2/3 lg:w-1/2 mr-6 rounded-lg bg-gray-300'>
           <p className='text-lg font-semibold mb-4 text-gray-900'>
             tyleroneil/AboutMe<span className='text-gray-600'>.md</span>
