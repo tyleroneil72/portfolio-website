dbbe179e308bab79021dc67f55ccfa1432a07ca1
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-18 16:29:07 -0500

    Create and Use SVG folder and Components

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Title from "../Title";
+import Github from "../svgs/Github";
+import Gear from "../svgs/Gear";
 import chatbot from "../../assets/images/chatbot.jpeg";
 import learningPath from "../../assets/images/learningPath.png";
 import hrPayroll from "../../assets/images/hrAndPayroll.png";
@@ -164,25 +166,13 @@ const Projects = () => {
               </div>
 
               <div className='flex justify-between'>
-                {/* Keep the GitHub link outside of the image */}
                 <a
                   href={project.githubLink}
                   target='_blank'
                   rel='noopener noreferrer'
                   className='text-blue-500 hover:underline'
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-5 w-5 mr-1 inline-block mb-1'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 0C4.477 0 0 4.477 0 10c0 4.418 2.865 8.166 6.839 9.49.5.092.682-.217.682-.482 0-.237-.009-.866-.014-1.7-2.782.603-3.369-1.338-3.369-1.338-.455-1.157-1.112-1.465-1.112-1.465-.908-.62.069-.608.069-.608 1.004.07 1.53 1.03 1.53 1.03.892 1.527 2.34 1.087 2.91.831.09-.645.35-1.087.636-1.337-2.22-.252-4.555-1.11-4.555-4.943 0-1.09.39-1.98 1.03-2.68-.103-.253-.446-1.27.097-2.647 0 0 .84-.269 2.75 1.025A9.61 9.61 0 0110 4.79c.855.004 1.715.115 2.52.34 1.91-1.294 2.75-1.025 2.75-1.025.544 1.377.202 2.394.1 2.647.64.7 1.03 1.59 1.03 2.68 0 3.842-2.337 4.688-4.565 4.936.36.31.68.922.68 1.855 0 1.338-.012 2.416-.012 2.746 0 .267.18.578.688.48C17.138 18.163 20 14.418 20 10c0-5.523-4.477-10-10-10z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
+                  <Github />
                   Github
                 </a>
                 {project.productionLink && (
@@ -192,6 +182,7 @@ const Projects = () => {
                     rel='noopener noreferrer'
                     className='text-blue-500 hover:underline'
                   >
+                    <Gear />
                     Production
                   </a>
                 )}
