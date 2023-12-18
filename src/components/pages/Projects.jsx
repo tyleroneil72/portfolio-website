20d55dae87921b70cd6294120c210b4c60c399bf
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-17 20:31:15 -0500

    Fill in project information

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -14,45 +14,52 @@ const Projects = () => {
       id: 1,
       title: "Chat Bot",
       image: headshot,
-      description: "Description of Project One",
-      githubLink: "link_to_github_project",
+      description:
+        "This is a chat bot built with React.js, Node.js and Express.js, integrated with OpenAI's API to provide natural language processing capabilities. The bot can engage in conversations with users, answer questions, and provide responses based on the context of the conversation.",
+      githubLink: "https://github.com/tyleroneil72/react-chat-bot",
     },
     {
       id: 2,
       title: "Learning Path Creator",
       image: headshot,
-      description: "Description of Project Two",
-      githubLink: "link_to_github_project",
+      description:
+        "The Learning Path Creator is a full stack web application designed to facilitate collaborative learning and knowledge sharing. This project when deployed operates on the LAMP stack, leveraging Linux, Apache web server, MySQL, and PHP. Additionally, Bootstrap is employed for frontend development, ensuring a visually appealing and responsive website.",
+      githubLink: "https://github.com/tyleroneil72/learning-path-creator",
     },
     {
       id: 3,
       title: "Hr & Payroll Management System",
       image: headshot,
-      description: "Description of Project Three",
-      githubLink: "link_to_github_project",
+      description:
+        "This project represents a Human Resources Payroll System developed in Java, utilizing JavaFX and Maven for its implementation. FXML and CSS were used for styling. Completed as part of an 'Application Development Using Java' course, the system facilitates employee management functionalities such as addition, editing, and deletion from the payroll. Additionally, it provides the capability to generate individual employee reports.",
+      githubLink: "https://github.com/tyleroneil72/hr-payroll-system",
     },
     {
       id: 4,
       title: "Sorting Algorithm Visualizer",
       image: headshot,
-      description: "Description of Project Four",
-      githubLink: "link_to_github_project",
-      productionLink: "link_to_production_project",
+      description:
+        "This project is a web application designed to visualize the workings of various sorting algorithms, including Bubble Sort, Selection Sort, Insertion Sort, Heap Sort, and Quick Sort. Through dynamic and interactive visualizations, users can observe the step-by-step execution of these algorithms and compare their performance metrics.",
+      githubLink:
+        "lhttps://github.com/tyleroneil72/sorting-algorithm-visualizer",
+      productionLink:
+        "https://htmlpreview.github.io/?https://github.com/tyleroneil72/sorting-algorithm-visualizer/blob/main/sorting_algorithms/index.html",
     },
     {
       id: 5,
       title: "Portfolio Website",
       image: headshot,
-      description: "Description of Project Five",
+      description:
+        "This is my portfolio website built using React (Vite), styled with Tailwind CSS, and hosted with Firebase. The site is designed to showcase my projects, skills, and experiences. Please note that this project is currently under active development. (This is the project you are currently viewing!)",
       githubLink: "link_to_github_project",
-      productionLink: "link_to_production_project",
     },
     {
       id: 6,
       title: "Miscellaneous Projects",
       image: headshot,
-      description: "Description of Project Six",
-      githubLink: "link_to_github_project",
+      description:
+        "Repo for my miscellaneous code, pet projects, or just code that doesn't fit into specific categories or projects.",
+      githubLink: "https://github.com/tyleroneil72/misc-code",
     },
   ];
 
@@ -70,8 +77,6 @@ const Projects = () => {
             key={project.id}
             className='border-2 border-gray-300 rounded-md p-6'
           >
-            {/* Project content */}
-            {/* Replace this with your project display */}
             <img
               src={project.image}
               alt={project.title}
