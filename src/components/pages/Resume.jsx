00f57b9bda68af66c954f3dff46cfef6007bfdd8
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-17 22:51:12 -0500

    Remove Resume Iframe

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -18,13 +18,13 @@ const Resume = () => {
         }`}
       >
         {/* TODO: Fill out, Maybe delete this, but if not needs to look nicer*/}
-        <div className='mb-8'>
+        {/* <div className='mb-8'>
           <iframe
             src='/resume.pdf'
             className='w-full h-96 border-2 border-gray-300 rounded-md shadow-md'
             title='Resume'
           />
-        </div>
+        </div> */}
         <a
           href='/resume.pdf'
           class='relative px-5 py-2 font-medium text-white group'
