2e821b8923925e44423cdc825a84903f17d478b3
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-17 20:49:02 -0500

    Add images to projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import Title from "../Title";
 import headshot from "../../assets/images/headshot.jpg";
-
+import chatbot from "../../assets/images/chatbot.jpeg";
+import learningPath from "../../assets/images/learningPath.png";
+import hrPayroll from "../../assets/images/hrAndPayroll.png";
+import sorting from "../../assets/images/sorting.jpeg";
 const Projects = () => {
   const [fadeIn, setFadeIn] = useState(false);
 
@@ -13,7 +16,7 @@ const Projects = () => {
     {
       id: 1,
       title: "Chat Bot",
-      image: headshot,
+      image: chatbot,
       description:
         "This is a chat bot built with React.js, Node.js and Express.js, integrated with OpenAI's API to provide natural language processing capabilities. The bot can engage in conversations with users, answer questions, and provide responses based on the context of the conversation.",
       githubLink: "https://github.com/tyleroneil72/react-chat-bot",
@@ -21,7 +24,7 @@ const Projects = () => {
     {
       id: 2,
       title: "Learning Path Creator",
-      image: headshot,
+      image: learningPath,
       description:
         "The Learning Path Creator is a full stack web application designed to facilitate collaborative learning and knowledge sharing. This project when deployed operates on the LAMP stack, leveraging Linux, Apache web server, MySQL, and PHP. Additionally, Bootstrap is employed for frontend development, ensuring a visually appealing and responsive website.",
       githubLink: "https://github.com/tyleroneil72/learning-path-creator",
@@ -29,7 +32,7 @@ const Projects = () => {
     {
       id: 3,
       title: "Hr & Payroll Management System",
-      image: headshot,
+      image: hrPayroll,
       description:
         "This project represents a Human Resources Payroll System developed in Java, utilizing JavaFX and Maven for its implementation. FXML and CSS were used for styling. Completed as part of an 'Application Development Using Java' course, the system facilitates employee management functionalities such as addition, editing, and deletion from the payroll. Additionally, it provides the capability to generate individual employee reports.",
       githubLink: "https://github.com/tyleroneil72/hr-payroll-system",
@@ -37,7 +40,7 @@ const Projects = () => {
     {
       id: 4,
       title: "Sorting Algorithm Visualizer",
-      image: headshot,
+      image: sorting,
       description:
         "This project is a web application designed to visualize the workings of various sorting algorithms, including Bubble Sort, Selection Sort, Insertion Sort, Heap Sort, and Quick Sort. Through dynamic and interactive visualizations, users can observe the step-by-step execution of these algorithms and compare their performance metrics.",
       githubLink:
