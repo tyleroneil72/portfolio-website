7dfa892851dd7177d5793d88586d2e2116fe181c
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-18 09:46:54 -0500

    Edit Margin and Padding

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -123,7 +123,7 @@ const Projects = () => {
       <Title title={"Projects"} />
 
       <div
-        className={`absolute mt-24 ml-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-8 py-8 transition-opacity duration-1000 ${
+        className={`absolute mt-28 ml-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-8 pb-8 transition-opacity duration-1000 ${
           fadeIn ? " opacity-100" : " opacity-0"
         }`}
       >
