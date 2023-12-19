b5bf4ea55a1a04b3da2c6f0a58785b514130a972
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-18 21:09:38 -0500

    Add Github and LinkedIn to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
+import Github from "./svgs/Github";
+import LinkedIn from "./svgs/LinkedIn";
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    // TODO: Fix footer to look nicer and have content
-    <footer className='fixed bottom-2 right-10 max-w-md mx-auto bg-slate-950 rounded-md pl-4 pr-4'>
-      <p className='text-slate-100'>&copy; Tyler O'Neil {currentYear}</p>
+    <footer className='fixed bottom-2 right-0 md:right-10 max-w-md mx-auto bg-slate-950 rounded-md pl-4 pr-4 py-2 w-auto'>
+      <p className='text-slate-100'>
+        <span className='flex'>
+          &copy; Tyler O'Neil {currentYear}
+          <a
+            href='https://github.com/tyleroneil72'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex items-center mx-4'
+          >
+            <Github /> Github
+          </a>
+          <a
+            href='https://linkedin.com/in/tyler-oneil-dev'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex items-center'
+          >
+            <LinkedIn /> LinkedIn
+          </a>
+        </span>
+      </p>
     </footer>
   );
 };
