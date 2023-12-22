cafbdc751709f7191f70eeae0dba846b83d00a55
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-22 18:15:50 -0500

    Add Email Icon

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Title from "../Title";
 import ContactForm from "../ContactForm";
+import { TfiEmail } from "react-icons/tfi";
 
 const ContactMe = () => {
   const [fadeIn, setFadeIn] = useState(false);
@@ -19,8 +20,11 @@ const ContactMe = () => {
 
       <div className={containerClasses}>
         <div className='bg-gray-300 rounded-lg shadow-md p-6 pb-10 w-full'>
-          <h2 className='text-2xl font-bold mb-4 text-gray-800'>
+          <h2 className='text-2xl font-bold mb-4 text-gray-800 flex items-center'>
             Let's Connect!
+            <span className='inline-block ml-2'>
+              <TfiEmail />
+            </span>
           </h2>
 
           <p className='text-lg mb-4 text-gray-800'>
@@ -28,7 +32,7 @@ const ContactMe = () => {
             feel free to reach out using the form below or my email address
             directly at{" "}
             <span className='text-blue-500'>tyleroneildev@gmail.com</span> and I
-            will get back to you as soon as possible.
+            will get back to you as soon as possible.{" "}
           </p>
 
           <ContactForm />
