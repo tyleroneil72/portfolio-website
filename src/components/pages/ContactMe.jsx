42e70cc647a26588a9ea75614e8a9572f29dfeb1
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-21 20:14:16 -0500

    Make header smaller

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -19,7 +19,7 @@ const ContactMe = () => {
 
       <div className={containerClasses}>
         <div className='bg-gray-100 rounded-lg shadow-md p-6'>
-          <h2 className='text-3xl font-bold mb-4 text-gray-800'>
+          <h2 className='text-2xl font-bold mb-4 text-gray-800'>
             Let's Connect!
           </h2>
 
