27b41bd73ae303cf91b1f7fb3116cdff796da7a7
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-23 11:24:21 -0500

    Add Subject in form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -62,7 +62,7 @@ const ContactForm = () => {
       <div className='hidden'>
         <input type='text' name='_gotcha' tabIndex='-1' autoComplete='off' />
       </div>
-      <div className='mb-4'>
+      <div className='mb-4 flex items-center gap-3'>
         <input
           type='text'
           placeholder='Your name'
@@ -70,12 +70,20 @@ const ContactForm = () => {
           className='focus:outline-none focus:ring w-full px-3 py-2 text-sm text-gray-600 placeholder-gray-400 bg-white border-2 border-gray-300 rounded shadow outline-none transition duration-300 ease-in-out'
           required
         />
+
+        <input
+          type='email'
+          placeholder='Your email'
+          name='email'
+          className='focus:outline-none focus:ring w-full px-3 py-2 text-sm text-gray-600 placeholder-gray-400 bg-white border-2 border-gray-300 rounded shadow outline-none transition duration-300 ease-in-out'
+          required
+        />
       </div>
       <div className='mb-4'>
         <input
-          type='email'
-          placeholder='Email'
-          name='email'
+          type='text'
+          placeholder='Subject'
+          name='subject'
           className='focus:outline-none focus:ring w-full px-3 py-2 text-sm text-gray-600 placeholder-gray-400 bg-white border-2 border-gray-300 rounded shadow outline-none transition duration-300 ease-in-out'
           required
         />
