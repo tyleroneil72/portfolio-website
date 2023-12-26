d7e3d508d3b83b313276da2a24c16abbe2cb0ff9
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-26 18:50:37 -0500

    Edit Blur Title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(() => {
     const handleBlur = () => {
       if (!isMobile) {
-        document.title = "Thank You For Visiting!";
+        document.title = "Thank You For Visiting! - Tyler O'Neil";
       }
     };
 
