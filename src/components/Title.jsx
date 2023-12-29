b3ef38a5ef406053f1b299dee54f1bbdb46c6e92
Tyler O'Neil <tyleroneildev@gmail.com>
2023-12-28 20:41:37 -0500

    Simplify Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import Typewriter from "./effects/Typewriter";
 
 const Title = ({ title }) => {
   return (
-    <div className='h-screen w-screen flex flex-col ml-[5.5rem]'>
+    <div className='flex flex-col ml-[5.5rem]'>
       <h1 className='text-4xl font-bold text-slate-100 mt-8 mb-4'>
         <Typewriter text={title} delay={70} infinite={false} />
       </h1>
