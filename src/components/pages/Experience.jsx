d4e312e1a9ddb6e3a5d52df347d5bf639ae2188e
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-04 17:00:50 -0500

    Edit Formatting

--- src/components/pages/Experience.jsx
+++ src/components/pages/Experience.jsx
@@ -24,7 +24,7 @@ const Experience = () => {
   }, []);
 
   const containerClasses = useMemo(() => {
-    return `absolute mt-28 ml-[5.5rem] mr-5 pb-14 pt-4 md:pt-0 transition-opacity duration-1000 text-gray-800 md:w-[calc(100%-7rem)] ${
+    return `absolute mt-28 ml-[4.5rem] mr-1 pb-14 pt-4 md:pt-0 transition-opacity duration-1000 text-gray-800 w-[calc(100%-5rem)] ${
       fadeIn ? "opacity-100" : "opacity-0"
     }`;
   }, [fadeIn]);
@@ -42,14 +42,14 @@ const Experience = () => {
       <Title title={"Experience and Skills"} />
 
       <div className={containerClasses}>
-        <section className='mb-8'>
-          <h2 className='text-2xl font-bold mb-3 flex justify-center md:justify-start text-gray-50'>
-            <FaBriefcase className='mr-2' />
+        <section className='mb-8 bg-gradient-to-r from-indigo-500 to-indigo-600 p-5 rounded-lg shadow-lg'>
+          <h2 className='text-3xl font-extrabold mb-4 flex justify-center md:justify-start text-white'>
+            <FaBriefcase className='mr-2 text-xl' />
             Work Experience
           </h2>
-          <div className='bg-gray-50 shadow-md rounded-md p-4'>
-            <p className='font-semibold flex items-center justify-between'>
-              <span className='flex items-center'>
+          <div className='bg-white rounded-md p-6 shadow-sm'>
+            <p className='font-semibold flex items-center justify-between border-b pb-3 mb-3'>
+              <span className='flex items-center text-gray-700'>
                 <img
                   src={ESI}
                   alt='ESI icon'
@@ -60,36 +60,32 @@ const Experience = () => {
                   href='https://www.exchangesolutions.com/'
                   target='_blank'
                   rel='noopener noreferrer'
-                  className='text-gray-50 bg-indigo-400 rounded px-1 py-1 hover:underline hover:text-white'
+                  className='text-blue-600 hover:text-blue-800 hover:underline'
                 >
-                  Software Developer - Intern @ Exchange Solutions Inc.{" "}
+                  Software Developer - Intern @ Exchange Solutions Inc.
                 </a>
               </span>
-              <span className='text-sm text-gray-700 ml-2'>
+              <span className='text-sm text-gray-500'>
                 May 2023 - September 2023
               </span>
             </p>
-            <ul className='list-disc ml-6'>
+            <ul className='list-disc ml-6 text-gray-700'>
               <li>
-                Created Node.js scripts to build proof of concept applications
-                for cutting-edge projects, including Retrieval Augmented AI
-                Agents, Digital Coupon Automation, and the Snowflake Native
-                Application Framework.
+                Created Node.js scripts for cutting-edge projects like Retrieval
+                Augmented AI Agents, Digital Coupon Automation, and Snowflake
+                Native Application Framework.
               </li>
               <li>
-                Collaborated with cross-functional teams to explore innovative
-                solutions, conduct experiments, and iterate on ideas to drive
-                the development of new technologies and applications.
+                Collaborated with teams for innovative solutions, conducting
+                experiments, and driving new technology development.
               </li>
               <li>
-                Worked closely with senior developers and researchers to create
-                prototypes, conduct feasibility studies, and validate concepts,
-                ensuring alignment with project goals and objectives.
+                Worked with senior developers on prototypes, feasibility
+                studies, and concept validation.
               </li>
             </ul>
-
-            <p className='font-semibold mt-2 flex items-center justify-between'>
-              <span className='flex items-center'>
+            <p className='font-semibold mt-4 flex items-center justify-between border-b pb-3 mb-3'>
+              <span className='flex items-center text-gray-700'>
                 <img
                   src={presentec}
                   alt='presentec icon'
@@ -100,42 +96,39 @@ const Experience = () => {
                   href='https://presentecsolutions.com/'
                   target='_blank'
                   rel='noopener noreferrer'
-                  className='text-gray-50 bg-indigo-400 rounded px-1 py-1 hover:underline hover:text-white'
+                  className='text-blue-600 hover:text-blue-800 hover:underline'
                 >
-                  Front-End Developer - Intern @ PresenTEC Solutions{" "}
+                  Front-End Developer - Intern @ PresenTEC Solutions
                 </a>
               </span>
-              <span className='text-sm text-gray-700 ml-2'>
+              <span className='text-sm text-gray-500'>
                 December 2022 - April 2023
               </span>
             </p>
-            <ul className='list-disc ml-6'>
+            <ul className='list-disc ml-6 text-gray-700'>
               <li>
-                Oversaw the ongoing maintenance and enhancements of the company
-                website using WordPress, ensuring it remained up-to-date and
-                functional.
+                Oversaw maintenance and enhancements of the company website
+                using WordPress.
               </li>
               <li>
-                Played a key role in selecting templates and shaping the user
-                interface to align with the company's branding, enhancing the
-                overall user experience.
+                Selected templates and shaped the user interface to align with
+                company branding.
               </li>
               <li>
-                Utilised HTML and CSS to fine-tune website layouts and styles,
-                optimising the site's performance and appearance
+                Utilized HTML and CSS for website layout and style optimization.
               </li>
             </ul>
           </div>
         </section>
 
-        <section className='mb-8'>
-          <h2 className='text-2xl font-bold mb-3 flex items-center justify-center md:justify-start text-gray-50'>
-            <FaGraduationCap className='mr-2' />
+        <section className='mb-8 bg-gradient-to-r from-indigo-500 to-indigo-600 p-5 rounded-lg shadow-lg'>
+          <h2 className='text-3xl font-extrabold mb-4 flex items-center justify-center md:justify-start text-white'>
+            <FaGraduationCap className='mr-2 text-xl' />
             Education
           </h2>
-          <div className='bg-gray-50 shadow-md rounded-md p-4'>
-            <p className='font-semibold flex items-center justify-between'>
-              <span className='flex items-center'>
+          <div className='bg-white rounded-md p-6 shadow-sm'>
+            <p className='font-semibold flex items-center justify-between border-b pb-3 mb-3'>
+              <span className='flex items-center text-gray-700'>
                 <img
                   src={gbc}
                   alt='GBC Image'
@@ -143,13 +136,13 @@ const Experience = () => {
                   loading='lazy'
                 />
                 Advanced Diploma, George Brown College, Computer Programming and
-                Analysis{" "}
+                Analysis
               </span>
-              <span className='text-sm text-gray-700 ml-2'>
+              <span className='text-sm text-gray-500'>
                 Graduating April 2025
               </span>
             </p>
-            <ul className='list-disc ml-6'>
+            <ul className='list-disc ml-6 text-gray-700'>
               <li>
                 Recognized for academic excellence and placed on the dean's list
                 with a 3.81 / 4.0 GPA (Fall 2022; Winter 2023; Fall 2023)
@@ -160,8 +153,8 @@ const Experience = () => {
                 Object Oriented Programming, Linux Essentials
               </li>
             </ul>
-            <p className='font-semibold mt-2 flex items-center justify-between'>
-              <span className='flex items-center'>
+            <p className='font-semibold mt-4 flex items-center justify-between border-b pb-3 mb-3'>
+              <span className='flex items-center text-gray-700'>
                 <img
                   src={uoft}
                   alt='UofT Image'
@@ -169,11 +162,11 @@ const Experience = () => {
                   loading='lazy'
                 />
                 Honours Bachelor of Science, University of Toronto, Computer
-                Science and Mathematics{" "}
+                Science and Mathematics
               </span>
-              <span className='text-sm text-gray-700 ml-2'>2019 - 2022</span>
+              <span className='text-sm text-gray-500'>2019 - 2022</span>
             </p>
-            <ul className='list-disc ml-6'>
+            <ul className='list-disc ml-6 text-gray-700'>
               <li>
                 Previously attended before transferring to George Brown College
               </li>
@@ -186,39 +179,44 @@ const Experience = () => {
           </div>
         </section>
 
-        <section className='mb-8'>
-          <h2 className='text-2xl font-bold mb-3 flex items-center justify-center md:justify-start text-gray-50'>
-            <FaLaptopCode className='mr-2' />
+        <section className='mb-8 bg-gradient-to-r from-indigo-500 to-indigo-600 p-5 rounded-lg shadow-lg'>
+          <h2 className='text-3xl font-extrabold mb-4 flex items-center justify-center md:justify-start text-white'>
+            <FaLaptopCode className='mr-2 text-xl' />
             Technical Skills
           </h2>
-          <div className='bg-gray-50 shadow-md rounded-md p-4'>
-            <ul className='list-disc list-inside'>
-              <li className='flex items-center'>
+          <div className='bg-white rounded-md p-6 shadow-sm'>
+            <ul className='space-y-4'>
+              <li className='flex items-center text-gray-700'>
                 <div className='text-2xl my-1'>
-                  <FaCode className='mr-2' />
-                </div>{" "}
-                Languages: Javascript/Typescript, Python, Java, C#, Bash, PHP,
-                SQL
-              </li>
-              <li className='flex items-center'>
-                <div className='text-2xl my-1'>
-                  <FaTools className='mr-2' />
+                  <FaCode className='text-green-500 mr-2 text-2xl' />
                 </div>
-                Technologies and Frameworks: HTML, CSS, Node.js, Express.js,
-                React.js, Vite, Tailwind CSS, Bootstrap, .NET
+                <span className='font-semibold'>Languages:</span>{" "}
+                Javascript/Typescript, Python, Java, C#, Bash, PHP, SQL
               </li>
-              <li className='flex items-center'>
+              <li className='flex items-center text-gray-700'>
                 <div className='text-2xl my-1'>
-                  <FaDatabase className='mr-2' />
-                </div>{" "}
-                Databases: MySQL, Snowflake
-              </li>
-              <li className='flex items-center'>
-                <div className='text-2xl my-1'>
-                  <FaTools className='mr-2' />
+                  <FaTools className='text-blue-500 mr-2' />
                 </div>
-                Development Tools: AWS, Linux, Git/Github, Firebase Hosting,
-                Wordpress, Jira, Confluence, Slack
+                <span className='font-semibold'>
+                  Technologies and Frameworks:
+                </span>{" "}
+                HTML, CSS, Node.js, Express.js, React.js, Vite, Tailwind CSS,
+                Bootstrap, .NET
+              </li>
+              <li className='flex items-center text-gray-700'>
+                <div className='text-2xl my-1'>
+                  <FaDatabase className='text-red-500 mr-2' />
+                </div>
+                <span className='font-semibold'>Databases:</span> MySQL,
+                Snowflake
+              </li>
+              <li className='flex items-center text-gray-700'>
+                <div className='text-2xl my-1'>
+                  <FaTools className='text-yellow-500 mr-2 text-2xl' />
+                </div>
+                <span className='font-semibold'>Development Tools:</span> AWS,
+                Linux, Git/Github, Firebase Hosting, Wordpress, Jira,
+                Confluence, Slack
               </li>
             </ul>
           </div>
