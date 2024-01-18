88bc1effed11795573855123a1ccd503edd896b2
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-17 23:06:02 -0500

    Remove react import

--- src/components/effects/Typewriter.jsx
+++ src/components/effects/Typewriter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Typewriter = ({ text, delay, infinite }) => {
   const [currentText, setCurrentText] = useState("");
