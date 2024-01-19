4cabe3d30eda46d4b10cc184b6e8b9073285a7fa
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-19 17:54:56 -0500

    Edit Title margin

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import Typewriter from "./effects/Typewriter";
 const Title = ({ title }) => {
   return (
     <div className='flex flex-col ml-[5.5rem]'>
-      <h1 className='text-4xl font-bold text-slate-100 mt-8 mb-4'>
+      <h1 className='text-4xl font-bold text-slate-100 mt-6 mb-4'>
         <Typewriter text={title} delay={70} infinite={false} />
       </h1>
     </div>
