2e3efcfb26f9ba9f915d0f6cdf9875daeafbea83
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-24 16:31:38 -0500

    Add Eslint

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -88,7 +88,7 @@ const projects = [
     description:
       "This is my portfolio website built using React (Vite), styled with Tailwind CSS, and hosted with Firebase. The site is designed to showcase my projects, skills, and experiences. Please note that this project is currently under active development. (This is the project you are currently viewing!)",
     githubLink: "https://github.com/tyleroneil72/portfolio-website",
-    techstack: [techStack[1], techStack[17], techStack[18]],
+    techstack: [techStack[1], techStack[17], techStack[18], techStack[24]],
   },
   {
     id: 7,
