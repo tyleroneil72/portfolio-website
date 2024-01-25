9dbb6c4fe650f76531516164ff3dace07dee8ecc
Tyler O'Neil <tyleroneildev@gmail.com>
2024-01-25 11:11:41 -0500

    Add todo

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -4,6 +4,11 @@ import Notification from "../effects/Notification.jsx";
 import { IconContext } from "react-icons";
 import { LuConstruction } from "react-icons/lu";
 
+/* 
+  TODO: Use react router to link to blog posts.
+        This page will display all blog posts at a high level
+        and will link to the individual blog posts.
+*/
 const Blog = () => {
   const [fadeIn, setFadeIn] = useState(false);
 
