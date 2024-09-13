9ea944100c126f2cbf806fb6f454c4b781d12e07
Tyler O'Neil <tyleroneildev@gmail.com>
2024-09-13 14:34:59 -0400

    Update tech stack images

--- src/assets/data/techStack.js
+++ src/assets/data/techStack.js
@@ -1,8 +1,8 @@
 const techStack = [
   "https://img.shields.io/badge/License-MIT-blue.svg",
-  "https://img.shields.io/badge/-ReactJs-61DAFB?logo=react&logoColor=white&style=for-the-badge",
+  "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
   "https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white",
-  "https://img.shields.io/badge/Express.js-404D59?style=for-the-badge",
+  "https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB",
   "https://img.shields.io/badge/Openai-404D59?style=for-the-badge",
   "https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white",
   "https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white",
